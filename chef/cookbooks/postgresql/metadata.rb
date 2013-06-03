a15691bcb8c9cfe9c33a95a95322f446c84bec2e
Steven De Coeyer <steven@banteng.be>
2013-06-03 07:33:53 +0200

    [postgresql] adds readme and metadata

---
+++ chef/cookbooks/postgresql/metadata.rb
@@ -0,0 +1,9 @@
+name             'postgresql'
+maintainer       'Banteng and Mr. Elephant'
+maintainer_email 'chef@banteng.be'
+license          'Apache 2.0'
+description      'Installs/Configures postgresql'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends           'apt'
+depends           'openssl'
